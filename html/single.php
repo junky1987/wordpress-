f756e6a73b09f4c28dc3768b874d77d02b1a6d49
junky1987 <cjyptot@gmail.com>
2020-07-10 05:31:34 +0900

    リンク画面のwordpressループ

--- html/single.php
+++ html/single.php
@@ -19,14 +19,15 @@
                 <div class="page-main" id="pg-newsDetail">
                   <div class="main-container">
                     <div class="main-wrapper">
+<?php
+if ( have_posts() ):
+  while ( have_posts() ) : the_post();
+?>
                       <div class="news">
-                        <time class="time">2019.01.14</time>
-                        <p class='title'>インド 消費者向けアプリ配信サービス大手のメカ・インディアと業務提携開始</p>
+                        <time class="time"><?php the_time( 'Y.m.d' ); ?></time>
+                        <p class='title'><?php the_title(); ?></p>
                         <div class="news-body">
-                          <p>
-                            パシフィックモール株式会社（以下、当社）は、インド共和国（以下、インド）において、Mecha-India（以下、メカ・インディア）との協業を開始します。<br />
-                            Mecha-Indiaは、インドにおける配車サービス大手であり、電子決済サービス事業のMECHA PAYを展開しています。今般、当社ショッピングモールにおいて、MECHA PAYを活用し、お客さまの利便性向上に関する様々な取り組みを行ってまいります。
-                          </p>
+                          <?php the_contents(); ?>
                         </div>
                       </div>
                       <div class="more-news">
@@ -37,6 +38,10 @@
                           <a class="another-link" href="#">PREV</a>
                         </div>
                       </div>
+<?php
+  endwhile;
+endif;
+?>
                     </div>
                   </div>
                 </div>
