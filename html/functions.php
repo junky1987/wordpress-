68f29745173373f2120dd036fa12e1bfd53ba7db
junky1987 <cjyptot@gmail.com>
2020-07-10 04:58:52 +0900

    カスタムメニュー有効化

--- html/functions.php
+++ html/functions.php
@@ -5,3 +5,11 @@ function my_enqueue_scripts() {
     wp_enqueue_style( 'my_styles', get_template_directory_uri(). '/assets/css/styles.css', array() );
 }
 add_action( 'wp_enqueue_scripts', 'my_enqueue_scripts' );
+// ヘッダー、フッターのカスタムメニュー化
+
+register_nav_menus(
+    array(
+        'place_global' => 'グローバル',
+        'place_footer' => 'フッターナビ',
+    )
+);
