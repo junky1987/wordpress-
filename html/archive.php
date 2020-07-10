bc8ff71f6d7ebe9a010b2a30b12fc77b06728b0d
junky1987 <cjyptot@gmail.com>
2020-07-11 01:32:13 +0900

    ニュースリリース遷移

---
+++ html/archive.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width,initial-scale=1" />
+  <meta name="keywords" content="共通キーワード" />
+  <meta name="description" content="共通ディスクリプション" />
+  <title>PACIFIC MALL DEVELOPMENT</title>
+  <link rel="shortcut icon" href="./assets/images/common/favicon.ico" />
+  <link href="https://fonts.googleapis.com/earlyaccess/notosansjapanese.css" rel="stylesheet" />
+  <link href="https://fonts.googleapis.com/css?family=Vollkorn:400i" rel="stylesheet" />
+  <link rel="stylesheet" type="text/css" href="./assets/css/styles.css" />
+  <script type="text/javascript" src="./assets/js/jquery-3.3.1.min.js"></script>
+  <script type="text/javascript" src="./assets/js/bundle.js"></script>
+</head>
+<body>
+<?php get_header(); ?>
+              <div class="page-inner full-width">
+                <div class="page-main" id="pg-news">
+                  <div class="main-container">
+                    <div class="main-wrapper">
+                      <div class="newsLists">
+<?php
+if  ( have_posts() ):
+  while ( have_posts() ) : the_post();
+?>
+                        <a class="news-link" href="<?php the_permalink(); ?>">
+                          <div class="news-body">
+                            <time class="release"><?php the_time('Y.m.d'); ?></time>
+                            <p class="title"><?php the_title(); ?></p>
+                          </div>
+                        </a>
+<?php
+  endwhile;
+endif;
+?>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+<?php get_footer(); ?>
+</body>
+</html>
